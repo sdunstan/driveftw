addf6631d600300269a85d5c0600930ec742c304
Max Quinones <maximilian.quinones@gmail.com>
2016-06-15 15:20:23 -0700

    new drive telementry class

---
+++ app/src/main/java/com/stevedunstan/driveftw/DriveTelementry.java
@@ -0,0 +1,15 @@
+package com.stevedunstan.driveftw;
+
+/**
+ * Created by M on 6/15/2016.
+ */
+public class DriveTelementry {
+
+    private Map<String, String> telementry;
+
+    
+
+    public DriveTelementry(Map<String, String> telementry) {
+        this.telementry = telementry;
+    }
+}
