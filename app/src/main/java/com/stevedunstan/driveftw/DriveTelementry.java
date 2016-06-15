bb1a72aa3b1f74f10ed0d87bff3394bc813881c6
Max Quinones <maximilian.quinones@gmail.com>
2016-06-15 16:30:37 -0700

    added  drive telementry

--- app/src/main/java/com/stevedunstan/driveftw/DriveTelementry.java
+++ app/src/main/java/com/stevedunstan/driveftw/DriveTelementry.java
@@ -1,15 +1,62 @@
 package com.stevedunstan.driveftw;
 
+import com.github.pires.obd.commands.ObdCommand;
+
+import java.util.Map;
+
 /**
  * Created by M on 6/15/2016.
  */
 public class DriveTelementry {
 
-    private Map<String, String> telementry;
 
-    
 
-    public DriveTelementry(Map<String, String> telementry) {
-        this.telementry = telementry;
+
+    private float throttle_position;
+    private float speed;
+    private float engineLoad;
+    private String engineRunTime;
+    private float fuelLevel;
+
+    public float getThrottle_position() {
+        return throttle_position;
     }
+
+    public void setThrottle_position(float throttle_position) {
+        this.throttle_position = throttle_position;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public float getEngineLoad() {
+        return engineLoad;
+    }
+
+    public void setEngineLoad(float engineLoad) {
+        this.engineLoad = engineLoad;
+    }
+
+    public String getEngineRunTime() {
+        return engineRunTime;
+    }
+
+    public void setEngineRunTime(String engineRunTime) {
+        this.engineRunTime = engineRunTime;
+    }
+
+    public float getFuelLevel() {
+        return fuelLevel;
+    }
+
+    public void setFuelLevel(float fuelLevel) {
+        this.fuelLevel = fuelLevel;
+    }
+
+
 }
