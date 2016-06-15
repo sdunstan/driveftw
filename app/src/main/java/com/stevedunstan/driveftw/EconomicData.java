835734fce0c6610c07bedb8c81c3ddaf0d00d168
computer <andrean360@gmail.com>
2016-06-15 12:50:38 -0700

    added shared preferences to save the econdata and username

--- app/src/main/java/com/stevedunstan/driveftw/EconomicData.java
+++ app/src/main/java/com/stevedunstan/driveftw/EconomicData.java
@@ -1,68 +1,95 @@
 package com.stevedunstan.driveftw;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.stevedunstan.driveftw.bluetooth.DeviceManager;
+
 /**
  * Created by computer on 6/15/2016.
- *
-
- //    Estimated yearly maintenance cost (tires, oil changes, etc.)  Cost of vehicle  Number of years you will keep it
-
- //
  */
 public class EconomicData {
     //cost estimates
-    double estMaintCostsPerYear;
-    double insuranceCostsPerYear;
-    double estMilesPerYear;
-    double totalOperatingCostperYear;
-    double estYearsKeep;
-    double costOfVehicle;
+    public float estMaintCostsPerYear;
+    public float insuranceCostsPerYear;
+    public float estMilesPerYear;
+    public float totalOperatingCostperYear;
+    public float estYearsKeep;
+    public float costOfVehicle;
+    public String userName;
 
+    private static String EST_MAINT_COST_PER_YEAR = "estMaintCostsPerYear";
+    private static String INSURANCE_COST_PER_YEAR = "insuranceCostsPerYear";
+    private static String EST_MILES_PER_YEAR = "estMilesPerYear";
+    private static String TOTAL_OPERATING_COST_PER_YEAR = "totalOperatingCostperYear";
+    private static String EST_YEARS_KEEP = "estYearsKeep";
+    private static String COST_OF_VEHICLE = "costOfVehicle";
+    private static String USER_NAME = "userName";
 
-    public double getEstMaintCostsPerYear() {
+    public void saveEconData(Context ctx) {
+        SharedPreferences sharedPreferences = ctx.getSharedPreferences(DeviceManager.DRIVE_FTW_SHARED_PREFERENCES, 0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putFloat(EST_MAINT_COST_PER_YEAR, estMaintCostsPerYear);
+        editor.putFloat(INSURANCE_COST_PER_YEAR, insuranceCostsPerYear);
+        editor.putFloat(EST_MILES_PER_YEAR, estMilesPerYear);
+        editor.putFloat(TOTAL_OPERATING_COST_PER_YEAR, totalOperatingCostperYear);
+        editor.putFloat(EST_YEARS_KEEP, estYearsKeep);
+        editor.putFloat(COST_OF_VEHICLE, costOfVehicle);
+
+        editor.commit();
+    }
+    public void saveUserName(Context ctx) {
+        SharedPreferences sharedPreferences = ctx.getSharedPreferences(DeviceManager.DRIVE_FTW_SHARED_PREFERENCES, 0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(USER_NAME, userName);
+        editor.commit();
+    }
+    
+    public float getEstMaintCostsPerYear() {
         return estMaintCostsPerYear;
     }
 
-    public void setEstMaintCostsPerYear(double estMaintCostsPerYear) {
+    public void setEstMaintCostsPerYear(float estMaintCostsPerYear) {
         this.estMaintCostsPerYear = estMaintCostsPerYear;
     }
 
-    public double getInsuranceCostsPerYear() {
+    public float getInsuranceCostsPerYear() {
         return insuranceCostsPerYear;
     }
 
-    public void setInsuranceCostsPerYear(double insuranceCostsPerYear) {
+    public void setInsuranceCostsPerYear(float insuranceCostsPerYear) {
         this.insuranceCostsPerYear = insuranceCostsPerYear;
     }
 
-    public double getEstMilesPerYear() {
+    public float getEstMilesPerYear() {
         return estMilesPerYear;
     }
 
-    public void setEstMilesPerYear(double estMilesPerYear) {
+    public void setEstMilesPerYear(float estMilesPerYear) {
         this.estMilesPerYear = estMilesPerYear;
     }
 
-    public double getTotalOperatingCostperYear() {
+    public float getTotalOperatingCostperYear() {
         return totalOperatingCostperYear;
     }
 
-    public void setTotalOperatingCostperYear(double totalOperatingCostperYear) {
+    public void setTotalOperatingCostperYear(float totalOperatingCostperYear) {
         this.totalOperatingCostperYear = totalOperatingCostperYear;
     }
 
-    public double getEstYearsKeep() {
+    public float getEstYearsKeep() {
         return estYearsKeep;
     }
 
-    public void setEstYearsKeep(double estYearsKeep) {
+    public void setEstYearsKeep(float estYearsKeep) {
         this.estYearsKeep = estYearsKeep;
     }
 
-    public double getCostOfVehicle() {
+    public float getCostOfVehicle() {
         return costOfVehicle;
     }
 
-    public void setCostOfVehicle(double costOfVehicle) {
+    public void setCostOfVehicle(float costOfVehicle) {
         this.costOfVehicle = costOfVehicle;
     }
 }
